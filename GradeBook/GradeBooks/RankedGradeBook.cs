66695a5c38f2510fc6ca12a9cf181cd56e5eda22
Kajtek2566 <Kajetan.stach@microsoft.wsei.edu.pl>
2023-04-03 21:13:57 +0200

    11/24 + Creating the RankedGradeBook class;  Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using GradeBook.GradeBooks;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
 
 namespace GradeBook.GradeBooks
 {
@@ -12,15 +17,36 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
-        public override void CalculateStatistics()
+        public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students.");
-                return;
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
             }
 
-            base.CalculateStatistics();
+            int top20Percent = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+            if (averageGrade >= grades[top20Percent - 1])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= grades[(top20Percent * 2) - 1])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= grades[(top20Percent * 3) - 1])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= grades[(top20Percent * 4) - 1])
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
